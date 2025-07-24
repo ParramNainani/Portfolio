a125757332b4f45d9ae0fcb1f6b3ccc5a95acd5b
ParramNainani <parramnainani@gmail.com>
2025-07-24 17:30:38 +0530

    PortFOLIO READY

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,15 @@ import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import './globals.css'
+import { Inter } from 'next/font/google'
 
 export const metadata: Metadata = {
   title: 'v0 App',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
@@ -25,7 +28,7 @@ html {
 }
         `}</style>
       </head>
-      <body>{children}</body>
+      <body className={`${inter.className} show-nav`}>{children}</body>
     </html>
   )
 }
