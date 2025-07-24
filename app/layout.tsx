2a35cadb19ae25c34d7bbdb1df56ea6ebf146b68
ParramNainani <parramnainani@gmail.com>
2025-07-24 17:18:53 +0530

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,9 @@ export default function RootLayout({
       <head>
         <style>{`
 html {
-  font-family: ${GeistSans.style.fontFamily};
-  --font-sans: ${GeistSans.variable};
-  --font-mono: ${GeistMono.variable};
+  font-family: "${GeistSans?.style?.fontFamily || 'sans-serif'}";
+  --font-sans: ${GeistSans?.variable || ''};
+  --font-mono: ${GeistMono?.variable || ''};
 }
         `}</style>
       </head>
